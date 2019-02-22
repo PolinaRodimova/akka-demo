b299edc54f89ea573d024fbdf534584a9598d0fe
pvlasenko <pvlasenko@roku.com>
2019-02-21 18:13:27 -0800

    sample 1 done

--- src/main/java/com/roku/akka/demo/sample1/App.java
+++ src/main/java/com/roku/akka/demo/sample1/App.java
@@ -1,13 +1,48 @@
 package com.roku.akka.demo.sample1;
 
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
+
 import java.util.Scanner;
 
 public class App {
 
 
+    static class Movie extends AbstractActor {
+        private final String title;
+        private int likes = 0;
+
+        Movie(String title) {
+            this.title = title;
+        }
+
+        static class LikeMsg{
+            private final String name;
+
+            LikeMsg(String name) {
+                this.name = name;
+            }
+        }
+
+        @Override
+        public Receive createReceive() {
+            return ReceiveBuilder.create()
+                    .match(LikeMsg.class, msg -> {
+                        likes++;
+                        System.out.println(msg.name + " liked " + title + "total=" + likes);
+                    })
+                    .build();
+        }
+    }
+
 
     public static void main(String[] args) {
-
+        ActorSystem system = ActorSystem.create("system");
+        ActorRef movieRef = system.actorOf(Props.create(Movie.class, "Die Hard"), "1");
+        movieRef.tell(new Movie.LikeMsg("Polina"), ActorRef.noSender());
 
         System.out.println("Enter to terminate");
         Scanner scanner = new Scanner(System. in);
